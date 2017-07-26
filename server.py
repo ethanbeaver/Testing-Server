4780e0e4e82561665a2ddd9590e8a1c2e362730c
Ethan Beaver <ethan.beaver@ahss.org>
2017-07-26 11:27:22 -0400

    Don't allow 100 status codes

--- server.py
+++ server.py
@@ -18,7 +18,7 @@ def status_Code_Helper(path):
     elif path == '/':
         print("Enter status code to return:")
         return input()
-    elif re.match('/[1-5]\d\d', path):
+    elif re.match('/[2-5]\d\d', path):
         return int(path[1:4])
     else:
         return 404
