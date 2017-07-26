325c23f0a3ffa7df9fc4e47f66cfe2b5714170f2
Ethan Beaver <ethan.beaver@ahss.org>
2017-07-26 11:18:32 -0400

    Major improvements to versatility

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 import time
 import BaseHTTPServer
 import textwrap
+import re
 
 
 HOST_NAME = 'localhost'
@@ -11,46 +12,68 @@ PORT_NUMBER = 8004
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_HEAD(s):
-        if s.path == '/accept':
-            s.send_response(200)
-        elif s.path == '/fail':
-            s.send_response(500)
+        if s.path in {'/accept', '/success'}:
+            status = 200
+        elif s.path in {'reject', '/fail'}:
+            status = 500
+        elif s.path == '/':
+            print("Enter status code to return:")
+            status = input()
+        elif re.match('/[1-5]\d\d', s.path):
+            status = s.path[1:3]
         else:
             s.send_response(404)
+            s.send_header("Content-type", "text/html")
+            s.end_headers()
+            s.wfile.write("404: Page Not Found")
+            return
+        s.send_response(status)
         s.send_header("Content-type", "text/html")
         s.end_headers()
     def do_GET(s):
-        if s.path == '/accept':
-            s.send_response(200)
-        elif s.path == '/fail':
-            s.send_response(500)
+        if s.path in {'/accept', '/success'}:
+            status = 200
+        elif s.path in {'reject', '/fail'}:
+            status = 500
+        elif s.path == '/':
+            print("Enter status code to return:")
+            status = input()
+        elif re.match('/[1-5]\d\d', s.path):
+            status = int(s.path[1:4])
         else:
             s.send_response(404)
             s.send_header("Content-type", "text/html")
             s.end_headers()
             s.wfile.write("404: Page Not Found")
             return
+        s.send_response(status)
         s.send_header("Content-type", "text/html")
         s.end_headers()
         s.wfile.write("<html><head><title>Ethan's Testing Server</title></head>")
-        s.wfile.write("<body><p>You've reached Ethan's Test Server. Thanks for using this service.</p>")
+        s.wfile.write("<body><h1>Status Code: %s</h1><p>You've reached Ethan's Test Server. Thanks for using this service.</p>" % status)
         s.wfile.write("<p>You accessed path: '%s' via a GET request</p>" % s.path)
         s.wfile.write("<p>Powered by Python</p></body></html>")
     def do_POST(s):
-        if s.path == '/accept':
-            s.send_response(200)
-        elif s.path == '/fail':
-            s.send_response(500)
+        if s.path in {'/accept', '/success'}:
+            status = 200
+        elif s.path in {'reject', '/fail'}:
+            status = 500
+        elif s.path == '/':
+            print("Enter status code to return:")
+            status = input()
+        elif re.match('/[1-5]\d\d', s.path):
+            status = s.path[1:3]
         else:
             s.send_response(404)
             s.send_header("Content-type", "text/html")
             s.end_headers()
             s.wfile.write("404: Page Not Found")
             return
+        s.send_response(status)
         s.send_header("Content-type", "text/html")
         s.end_headers()
         s.wfile.write("<html><head><title>Ethan's Testing Server</title></head>")
-        s.wfile.write("<body><p>You've reached Ethan's Test Server. Thanks for using this service.</p>")
+        s.wfile.write("<body><h1>Status Code: %s</h1><p>You've reached Ethan's Test Server. Thanks for using this service.</p>" % status)
         data = s.rfile.read(int(s.headers['Content-Length']))
         s.wfile.write("<p>You sent data: %s</p>" % data)
         wrapper = textwrap.TextWrapper(initial_indent="* ", subsequent_indent="*   ")
